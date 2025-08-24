b343399412f8d73bb1be5eb737f859c3defaf820
Piterxus <piterxus@gmail.com>
2025-08-24 17:28:20 +0200

    feat(auth): add Register component

--- react-persenaut/src/App.jsx
+++ react-persenaut/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import Demo from './components/Demo';
+import RegisterComponent from './components/Register';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/demo" element={<Demo />} />
+        <Route path="/register" element={<RegisterComponent />}/>
+
       </Routes>
     </Router>
   );
